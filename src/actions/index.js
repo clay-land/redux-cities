b81b7ed99818dca69225a3b5859306bc9f0ba3b0
Clayton Landry <clayton.landry@gmail.com>
2021-07-15 14:46:59 -0400

    cities with redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 // TODO: add and export your own actions
+import cities from '../cities';
+
+const setCities = () => {
+  // TODO: Api call! For now, simulate a DB
+  return {
+    type: 'SET_CITIES',
+    payload: cities
+  };
+};
+
+export default setCities;
--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -2,22 +2,44 @@
 /* eslint-disable react/jsx-indent */
 /* eslint-disable indent */
 import React from 'react';
-import City from './city';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-const CityList = ({ cities }) => {
-    return (
-        <div className="cities">
-            {cities.map((city) => {
-                return (
-                <City
-                name={city.name}
-                key={city.address}
-                address={city.address}
-                slug={city.slug}
-                />);
-            })}
-        </div>
+import City from './city';
+import setCities from '../actions';
+
+class CityList extends React.Component {
+    componentWillMount() {
+        this.props.setCities();
+    }
+    render() {
+        return (
+            <div className="cities">
+                {this.props.cities.map((city) => {
+                    return (
+                    <City
+                    name={city.name}
+                    key={city.address}
+                    address={city.address}
+                    slug={city.slug}
+                    />);
+                })}
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(
+        { setCities },
+        dispatch
     );
 };
 
-export default CityList;
+const mapStateToProps = (state) => {
+    return {
+        cities: state.cities
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
